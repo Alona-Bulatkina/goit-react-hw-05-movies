7543f4ee7380fc0ef817eb129f9848ae867aff5d
Alona-Bulatkina <83859343+Alona-Bulatkina@users.noreply.github.com>
2022-03-16 15:25:48 +0200

    add index.html

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+import AppBar from './components/AppBar/AppBar';
+// import Container
+
+
+export default function App() {
+  return (
+    // <Container>
+      <AppBar />
+    // </Container>
+  );
+}
